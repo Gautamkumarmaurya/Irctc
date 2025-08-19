405f4d937fbb6c739111ae5a69f2abcb1983a066
Gautam Kumar <116151957+Gautamkumarmaurya@users.noreply.github.com>
2025-08-20 01:31:54 +0530

    Update TrainRepository.java

--- src/main/java/com/subString/irctc/Repository/TrainRepository.java
+++ src/main/java/com/subString/irctc/Repository/TrainRepository.java
@@ -3,7 +3,7 @@ package com.subString.irctc.Repository;
 import com.subString.irctc.Entity.Train;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TrainRepository extends JpaRepository<Train, Long> {
+public interface TrainRepository extends JpaRepository<Train, String> {
 
 
 }
